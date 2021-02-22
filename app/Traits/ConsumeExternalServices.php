1912017846b66260360b8695b13011bf60eff1a6
gichuhibrian <brianchegegichuhi@gmail.com>
2021-02-22 22:57:14 +0300

    Added consumable routes as well as guzzle trait

---
+++ app/Traits/ConsumeExternalServices.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Traits;
+
+use GuzzleHttp\Client;
+
+trait ConsumeExternalServices
+{
+    /**
+     * Return string
+     */
+    public function performRequest($method, $requestUrl, $form_params, $headers)
+    {
+        $client = new Client([
+            'base_uri' => $this->baseUri,
+        ]);
+
+        $response = $client->request($method, $requestUrl, ['form_params' => 
+        $form_params, 'headers' => $headers]);
+
+        return $response->getBody()->getContents();
+    }
+}
